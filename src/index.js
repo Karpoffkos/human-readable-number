a32b5075201859f77860d18bd02d57e2bab6801e
Konstantsin Karpov <karpoffkos@mal.ru>
2023-01-25 09:22:09 +0300

    feat:ddd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+/* module.exports =  */function toReadable (number) {
+   
+
+    let low20 = ["ziro", "one", "two","three","four","five","six","seven","eight","nine", 'ten', 'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    let decade= [0, 0, "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
+    let single = number%10;
+    let hundred = Math.floor(number/100);
+    console.log(single, 8);
+    console.log(hundred, 9);
+    if (number < 20){ return low20[number] };
+    if (number < 100){ return single ? decade[Math.floor(number/10)] + " " + low20[single]  : decade[Math.floor(num/10)]};
+    if (number < 1000){ return low20[hundred] + "hundred"+ toReadable(number - hundred)};
+     
+   
 }
+toReadable (999)
