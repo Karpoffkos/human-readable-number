aed16f4f26a4bd2d03e7f79bd4f463f9579170ef
Konstantsin Karpov <karpoffkos@mal.ru>
2023-01-25 09:55:38 +0300

    feat:code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-/* module.exports =  */function toReadable (number) {
+module.exports = function toReadable (number) {
    
 
     let low20 = ["ziro", "one", "two","three","four","five","six","seven","eight","nine", 'ten', 'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let decade= [0, 0, "twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
     let single = number%10;
     let hundred = Math.floor(number/100);
-    console.log(single, 8);
-    console.log(hundred, 9);
-    if (number < 20){ return low20[number] };
-    if (number < 100){ return single ? decade[Math.floor(number/10)] + " " + low20[single]  : decade[Math.floor(num/10)]};
-    if (number < 1000){ return low20[hundred] + "hundred"+ toReadable(number - hundred)};
+/*     console.log(single, low20[single], 8);
+    console.log(hundred,  9); */
+    if (number < 20){ 
+    /*     console.log(11); */
+        return low20[number] };
+    if (number < 100){ 
+       /*  console.log(14); */
+        return single ? (decade[Math.floor(number/10)] + " " + low20[single])  : decade[Math.floor(number/10)]};
+    if (number < 1000){ 
+      /*   console.log(17); */
+        return low20[hundred] + "hundred"+ toReadable(number%100)}; 
      
    
 }
-toReadable (999)
